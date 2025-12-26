ee93cc527d8c4a539465a5642f57e6f89d0671b5
ShubhSam7 <shubhsamaiya7@gmail.com>
2025-12-26 11:49:03 +0530

    Initial commit: Second Brain app with Prisma 7 + Supabase backend and React frontend

--- Second_Brain_Backend/dist/config.js
+++
@@ -1,4 +0,0 @@
-import dotenv from "dotenv";
-dotenv.config();
-export const JWT_SECRET = process.env.JWT_SECRET || "shubh7124";
-export const PORT = process.env.PORT || 3000;
--- Second_Brain_Backend/dist/utils.js
+++
@@ -1,9 +0,0 @@
-export function random(len) {
-    let options = "qwertyuiop12345asdfghnbvcx";
-    let length = options.length;
-    let ans = "";
-    for (let i = 0; i < len; i++) {
-        ans += options[Math.floor(Math.random() * length)];
-    }
-    return ans;
-}
